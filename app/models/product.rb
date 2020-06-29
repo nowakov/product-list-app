57a9064c239215840ac1081b056905c71d8bfcf4
Mateusz Nowakowski <nowakov@gmail.com>
2020-06-29 18:38:49 +0200

    Add validations to Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class Product < ApplicationRecord
+  validates :name, presence: true, uniqueness: { case_sensitive: true }
+  validates :price, presence: true, numericality: true
 end
